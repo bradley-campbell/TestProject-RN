229e112533a0eb378fc338f900b83f783489ed70
frenger88 <64667571+frenger88@users.noreply.github.com>
2021-03-10 20:33:24 -0500

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+
+        <TouchableOpacity
+          onPress={() => alert("Hello World!")}
+          style={styles.button}
+        >
+          <Text style={styles.text}>Pick a photo</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "blue",
+  },
+  text: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
